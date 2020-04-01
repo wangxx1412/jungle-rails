c99e37b3654369432c58d5876ce843cc77546fee
Xiaoxuan Wang <wangxiaoxuan4869@gmail.com>
2020-04-01 02:27:06 +0000

    Complete sold out badge

--- db/seeds.rb
+++ db/seeds.rb
@@ -47,7 +47,7 @@ cat1.products.create!({
   name:  'Women\'s Zebra pants',
   description: Faker::Hipster.paragraph(4),
   image: open_asset('apparel2.jpg'),
-  quantity: 18,
+  quantity: 0,
   price: 124.99
 })
 
@@ -55,7 +55,7 @@ cat1.products.create!({
   name:  'Hipster Hat',
   description: Faker::Hipster.paragraph(4),
   image: open_asset('apparel3.jpg'),
-  quantity: 4,
+  quantity: 0,
   price: 34.49
 })
 
