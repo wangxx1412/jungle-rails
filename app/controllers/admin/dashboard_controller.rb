7520c4d8138eb9c0c472bb64043a46604687d392
Xiaoxuan Wang <wangxiaoxuan4869@gmail.com>
2020-04-01 20:23:33 +0000

    Add dashboard count

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,13 @@
 class Admin::DashboardController < ApplicationController
-  http_basic_authenticate_with name: "Jungle", password: "book"
+  http_basic_authenticate_with name: ENV["NAME"], password: ENV["PASSWORD"]
+
   def show
+    fetch_data
   end
+
+  private
+    def fetch_data
+      @product_count = Product.all.count()
+      @category_count = Category.all.count()
+    end
 end
